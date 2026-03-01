b1aaec7d9ba39f6ada10a41b322ab3c9bd277090
Cheyin L <cheyinl@gmail.com>
2026-03-01 09:48:12 +0800

    refactor: improve readability

--- identcode3131/mask.go
+++ identcode3131/mask.go
@@ -6,11 +6,11 @@ import (
 	"github.com/nangantata/go-identcode/internal"
 )
 
-func MakeIdentMask(seed []byte) (mask [16]uint32) {
+func MakeIdentMask(seed []byte) (identMasks [16]uint32) {
 	chksum := internal.Sum512(seed, 6)
 	b := chksum[:]
 	for idx := range 16 {
-		mask[idx] = uint32(binary.LittleEndian.Uint32(b[idx*4:]) & 0x7FFFFFFF)
+		identMasks[idx] = uint32(binary.LittleEndian.Uint32(b[idx*4:]) & 0x7FFFFFFF)
 	}
 	return
 }
--- identcode6331/mask.go
+++ identcode6331/mask.go
@@ -6,16 +6,16 @@ import (
 	"github.com/nangantata/go-identcode/internal"
 )
 
-func MakeIdentMask(seed []byte) (mask [16]uint64) {
+func MakeIdentMask(seed []byte) (identMasks [16]uint64) {
 	chksum := internal.Sum512(seed, 3)
 	b := chksum[:]
 	for idx := range 8 {
-		mask[idx] = uint64(binary.LittleEndian.Uint64(b[idx*8:]) & 0x7FFFFFFFFFFFFFFF)
+		identMasks[idx] = uint64(binary.LittleEndian.Uint64(b[idx*8:]) & 0x7FFFFFFFFFFFFFFF)
 	}
 	chksum = internal.Sum512(b, 5)
 	b = chksum[:]
 	for idx := range 8 {
-		mask[idx+8] = uint64(binary.LittleEndian.Uint64(b[idx*8:]) & 0x7FFFFFFFFFFFFFFF)
+		identMasks[idx+8] = uint64(binary.LittleEndian.Uint64(b[idx*8:]) & 0x7FFFFFFFFFFFFFFF)
 	}
 	return
 }
